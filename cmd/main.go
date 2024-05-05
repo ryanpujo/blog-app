a2d298bd31c313c81069550825d92986cb616546
ryan pujo <pujotriantoro@gmail.com>
2024-05-05 20:40:40 +0700

    movind database connection code into its own package

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/ryanpujo/blog-app/database"
 	"github.com/ryanpujo/blog-app/internal/registry"
 	"github.com/ryanpujo/blog-app/internal/route"
 )
 
 func main() {
-	registry := registry.New(EstablishDBConnectionWithRetry())
+	registry := registry.New(database.EstablishDBConnectionWithRetry())
 	app := Application(WithPort(4000))
 	app.Serve(route.Route(registry.NewAppController()))
 }
